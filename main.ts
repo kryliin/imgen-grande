c371ecf9df6c0712b19cbb9b030e3a50f05d6273
Christian Saliwonczyk <kryliin@gmail.com>
2020-08-09 11:39:08 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,17 @@
 let imagen_grande = images.createBigImage(`
-    . . . . . . # . . .
-    . . # . . # # . . .
-    . . . # . # . . . .
+    . . . # . . # . . .
+    . . . # . . # . . .
+    . . . # . . # . . .
+    . . . # . . # . . .
     . . . . # # . . . .
-    . . . . . . . . . .
     `)
-imagen_grande = 3
+imagen_grande.plotImage(3);
+// imagen_grande.showImage(0)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
-        imagen_grande += -1
+        imagen_grande.plotImage(-1);
     } else if (input.buttonIsPressed(Button.B)) {
-        imagen_grande += 1
+  imagen_grande.plotImage(+1);
     }
-    imagen_grande.showImage(0)
+    imagen_grande.plotImage(3);
 })
